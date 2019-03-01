c6a7a76d80ea056d450e2c071a7afe6dfefeed92
YianChen <ianchen0419@gmail.com>
2019-03-01 23:18:42 +0800

    all page

---
+++ inc/js/base.js
@@ -0,0 +1,61 @@
+var headerXhr=new XMLHttpRequest();
+
+headerXhr.open('GET', 'header.html', true);
+headerXhr.send();
+
+headerXhr.onreadystatechange=function(){
+	if(headerXhr.readyState==4 && headerXhr.status==200){
+
+		header.innerHTML = headerXhr.responseText;
+
+
+	}
+};
+
+// menu.html load
+var menuXhr=new XMLHttpRequest();
+
+menuXhr.open('GET', 'menu.html', true);
+menuXhr.send();
+
+menuXhr.onreadystatechange=function(){
+
+	
+	if(menuXhr.readyState==4 && menuXhr.status==200){
+
+		menu.innerHTML = menuXhr.responseText;
+		if(menuXhr.responseText){
+			menuHighlight();
+		}
+
+	}
+
+};
+
+//footer.html load
+var footerXhr=new XMLHttpRequest();
+
+footerXhr.open('GET', 'footer.html', true);
+footerXhr.send();
+
+footerXhr.onreadystatechange=function(){
+	if(footerXhr.readyState==4 && footerXhr.status==200){
+
+		footer.innerHTML = footerXhr.responseText;
+
+	}
+};
+
+//menu highlight
+var nowPath=location.pathname.split('/').pop();
+function menuHighlight(){
+
+	var visitedMenu=document.querySelector('nav a[href*="' + nowPath + '"]');
+
+	if(visitedMenu){
+		visitedMenu.closest('.main-list').classList.add('active');
+	}else{
+		document.querySelector('nav ul li').classList.add('active');
+	}
+	
+}
